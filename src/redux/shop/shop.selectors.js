a9ae026bfe41b3b80c7c276f0135dd11a7ca4cbf
hsimao <e087754958@gmail.com>
2020-02-22 14:36:52 +0800

    perf: shop data array 改成物件, 使用 hash tables 優化 collection page 索引速度

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,13 +1,5 @@
 import { createSelector } from 'reselect';
 
-const COLLECTION_ID_MAP = {
-  hats: 1,
-  sneakers: 2,
-  jackets: 3,
-  womens: 4,
-  mens: 5,
-};
-
 const selectShop = state => state.shop;
 
 export const selectCollections = createSelector(
@@ -15,11 +7,9 @@ export const selectCollections = createSelector(
   shop => shop.collections,
 );
 
-// mapping url string name 與 collection id
-// shop/jackets <=> id: 3
+// mapping url string name 與 collection object key
 export const selectCollection = collectionUrlParam =>
-  createSelector([selectCollections], collections =>
-    collections.find(
-      collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam],
-    ),
+  createSelector(
+    [selectCollections],
+    collections => collections[collectionUrlParam],
   );
