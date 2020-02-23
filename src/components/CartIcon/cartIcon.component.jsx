e9a7870d71acae032bafa00ee13cf8ff6291769e
hsimao <e087754958@gmail.com>
2020-02-23 14:34:22 +0800

    refactor: cartIcon style 改用 styled-component

--- src/components/CartIcon/cartIcon.component.jsx
+++ src/components/CartIcon/cartIcon.component.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
-import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
 import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 
-import classes from './cartIcon.module.scss';
+import {
+  CartIconContainer,
+  ShoppingIcon,
+  ItemCountContainer,
+} from './cartIcon.styles';
 
 const CartIcon = ({ toggleCartHidden, itemCount }) => {
   return (
-    <div className={classes.cartIcon} onClick={toggleCartHidden}>
-      <ShoppingIcon className={classes['shopping-icon']} />
-      <span className={classes['item-count']}> {itemCount} </span>
-    </div>
+    <CartIconContainer onClick={toggleCartHidden}>
+      <ShoppingIcon />
+      <ItemCountContainer> {itemCount} </ItemCountContainer>
+    </CartIconContainer>
   );
 };
 
