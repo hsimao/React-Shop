3559b730c02b6db17c0351d85518db726da479bb
hsimao <e087754958@gmail.com>
2020-03-07 14:02:07 +0800

    refactor: shop sagas 引入 root-saga 改用統一function shopSagas

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,4 +1,4 @@
-import { takeEvery, call, put } from 'redux-saga/effects';
+import { takeEvery, call, put, all } from 'redux-saga/effects';
 
 import {
   firestore,
@@ -32,4 +32,8 @@ export function* fetchCollectionsStart() {
     ShopActionTypes.FETCH_COLLECTIONS_START,
     fetchCollectionsAsync,
   );
+}
+
+export function* shopSagas() {
+  yield all([call(fetchCollectionsStart)]);
 }
